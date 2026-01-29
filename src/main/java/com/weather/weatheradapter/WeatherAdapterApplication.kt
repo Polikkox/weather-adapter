a49686ac1ae27e46044d18149d21b59570c0e60c
Damian Targosz <damian.targosz@pl.abb.com>
2026-01-29 15:49:02 +0100

    fix error

--- src/main/java/com/weather/weatheradapter/WeatherAdapterApplication.kt
+++ src/main/java/com/weather/weatheradapter/WeatherAdapterApplication.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class WeatherAdapterApplication
 
-fun main() {
-    runApplication<WeatherAdapterApplication>()
+fun main(args: Array<String>) {
+    runApplication<WeatherAdapterApplication>(*args)
 }
